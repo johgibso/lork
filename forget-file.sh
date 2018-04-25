6e43f6f15cacbad4f4d065ce8412daec1de948a2
John Gibson <johgibso@gmail.com>
2018-04-24 22:51:32 -0400

    Cautionary comment.

--- forget-file.sh
+++ forget-file.sh
@@ -3,6 +3,10 @@
 # delete it from disk.
 # This must be invoked from within the top-level repo directory,
 # with an appropriate relative path for the file name.
+#
+# Every time I've done this, it hoses people's repos, so that they
+# have to clone the repo again.
+#
 git filter-branch --force --index-filter \
 	"git rm --cached --ignore-unmatch $1" \
 	--prune-empty --tag-name-filter cat -- --all
